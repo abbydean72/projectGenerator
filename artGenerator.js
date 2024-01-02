c45d2c7f2f637bb05611214ad0d909ae014bc7eb
Abby Dean <100309092+abbydean72@users.noreply.github.com>
2024-01-01 21:25:17 -0700

    first commit. Added object containing options for project generator

---
+++ artGenerator.js
@@ -0,0 +1,5 @@
+const message = {
+    subject: ["someone you care about", "a fictional character", "an original character", "a landscape"],
+    style: ["anime", "classical", "abstract", "realism"],
+    medium: ["paint", "fiber", "pencil", "digital art", "woodworking"],
+};
